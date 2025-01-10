e4e84b030edb4f3ca8135e9d966ea9ef98b8fec3
Madhu-shree1243 <114743017+Madhu-shree1243@users.noreply.github.com>
2025-01-10 08:57:07 +0530

    oops

---
+++ Oopss.java
@@ -0,0 +1,34 @@
+public class Oopss{
+ public static void main(String[] args){
+     Pen p1 = new Pen(); //constructor... created a pen object called p1
+     p1.setColor("Blue");
+     System.out.println(p1.color);
+     p1.setTip(5);
+     System.out.println(p1.tip);
+     //p1.setColor("Yellow");
+     p1.color = "Yellow";
+     System.out.println(p1.color);
+ }
+}
+class Pen{
+     // prop+fun
+     String color;
+     int tip;
+
+     void setColor(String newColor){
+          color= newColor;
+     }
+     void setTip(int newTip){
+          tip = newTip;
+     }
+}
+
+// //class Student{
+//      String name;
+//      int age;
+//      float percentage; //cgpa
+
+//      void calcPercentage(int phy,int chem,int maths){
+//           percentage = (phy+chem+maths)/3;
+//      }
+// }
